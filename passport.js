b1b5d74c41eede9cfb3990138d93f4b202f45191
haipham <phamvanhaid512@gmail.com>
2023-10-03 08:46:07 +0700

    b

--- passport.js
+++ passport.js
@@ -1,26 +1,26 @@
-import passport from 'passport';
-import db from  './models';
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// import passport from 'passport';
+// import db from  './models';
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-require('dotenv').config()
- passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/users/google/callback"
-  },
-  async function(accessToken, refreshToken, profile, cb) {
-      //them User vào db
-      if(profile?.id) {
-        // const userId = parseInt(profile.id, 10);
-        await db.User.findOrCreate({
-          where:{fullname:profile.id},
-          defaults:{
-            email:profile.emails[0]?.value,
-            // typeLogin:profile?.provider
-          }
-        })
-      }
-    // console.log(profile); 
-    return cb(null, profile);
-  }
-));
+// require('dotenv').config()
+//  passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/api/users/google/callback"
+//   },
+//   async function(accessToken, refreshToken, profile, cb) {
+//       //them User vào db
+//       if(profile?.id) {
+//         // const userId = parseInt(profile.id, 10);
+//         await db.User.findOrCreate({
+//           where:{fullname:profile.id},
+//           defaults:{
+//             email:profile.emails[0]?.value,
+//             // typeLogin:profile?.provider
+//           }
+//         })
+//       }
+//     // console.log(profile); 
+//     return cb(null, profile);
+//   }
+// ));
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const UserController = require('../controllers/UserController.js');
 const authController = require('./../controllers/authController.js');
-const passport = require('passport');
 require('dotenv').config();
-require('../passport');
-
 const router = express.Router();
-router.get('/google',passport.authenticate('google', { scope: ['profile','email'], session: false }));
-router.get('/google/callback', (req, res, next) => {
-    passport.authenticate('google', (err, profile) => {
-       req.user = profile;
-       console.log("profile",profile);
-       next();
-    })(req, res, next)
-}, (req, res) => {
-    // Successful authentication, redirect home.
-    res.redirect(`${process.env.URL_CLIENT}/loginGoogle/${req.user?.id}`);
-});
-router.post('/loginGoogle', UserController.loginGoogle);
+// router.get('/google',passport.authenticate('google', { scope: ['profile','email'], session: false }));
+// router.get('/google/callback', (req, res, next) => {
+//     passport.authenticate('google', (err, profile) => {
+//        req.user = profile;
+//        console.log("profile",profile);
+//        next();
+//     })(req, res, next)
+// }, (req, res) => {
+//     // Successful authentication, redirect home.
+//     res.redirect(`${process.env.URL_CLIENT}/loginGoogle/${req.user?.id}`);
+// });
+// router.post('/loginGoogle', UserController.loginGoogle);
 router.post("/login", UserController.login);
 router.post("/signUp", UserController.signUp);
 
